20ff328eb18fe1be404fe3789b998a171a2a340c
Alexandre Beaulieu <alex@segfault.me>
2020-01-30 15:37:22 -0500

    feat: Added ServicePointManager technique. (Untested)

--- src/main.js
+++ src/main.js
@@ -15,8 +15,8 @@
  * In the .NET Core case, the HttpClient's `SendAsync` implementation is hooked to 
  * inject * a default HttpClientHandler that does not perform pinning. 
  *
- * [TODO] In the .NET Framework case, the System.Net.ServicePointerManager's is hooked
- *        to always return NULL and forcefully set to NULL in order to reset it.
+ * In the .NET Framework case, the System.Net.ServicePointerManager's is hooked
+ * to always return NULL and forcefully set to NULL in order to reset it.
  *
  * @note  Validation still happens so the certificate must be valid.
  */
@@ -28,30 +28,71 @@ const mono = MonoApi.module
 let status = Memory.alloc(0x1000);
 let http = MonoApi.mono_assembly_load_with_partial_name(Memory.allocUtf8String('System.Net.Http'), status);
 let img = MonoApi.mono_assembly_get_image(http);
+let hooked = false;
 
-// Construct a default HttpClientHandler to inject in HttpMessageInvoker instances.
+// Mono 6.0+: Construct a default HttpClientHandler to inject in HttpMessageInvoker instances.
 let kHandler = MonoApi.mono_class_from_name(img, Memory.allocUtf8String('System.Net.Http'), Memory.allocUtf8String('HttpClientHandler'));
-let ctor = MonoApiHelper.ClassGetMethodFromName(kHandler, 'CreateDefaultHandler');
-let pClientHandler = MonoApiHelper.RuntimeInvoke(ctor, NULL); // Static method -> instance = NULL.
-console.log(`[+] Created Default HttpClientHandler @ ${pClientHandler}`);
+if (kHandler) {
+    let ctor = MonoApiHelper.ClassGetMethodFromName(kHandler, 'CreateDefaultHandler');
+    let pClientHandler = MonoApiHelper.RuntimeInvoke(ctor, NULL); // Static method -> instance = NULL.
+    console.log(`[+] Created Default HttpClientHandler @ ${pClientHandler}`);
 
-// Hook HttpMessageInvoker.SendAsync
-let kInvoker = MonoApi.mono_class_from_name(img, Memory.allocUtf8String('System.Net.Http'), Memory.allocUtf8String('HttpMessageInvoker'));
+    // Hook HttpMessageInvoker.SendAsync
+    let kInvoker = MonoApi.mono_class_from_name(img, Memory.allocUtf8String('System.Net.Http'), Memory.allocUtf8String('HttpMessageInvoker'));
 
-console.log('[+] Hooking HttpMessageInvoker.SendAsync');
-MonoApiHelper.Intercept(kInvoker, 'SendAsync', {
-  onEnter: (args) => {
-    console.log(`[*] HttpClientHandler.SendAsync called`);
+    MonoApiHelper.Intercept(kInvoker, 'SendAsync', {
+	onEnter: (args) => {
+	    console.log(`[*] HttpClientHandler.SendAsync called`);
 
-    let self = args[0];
-    let handler = MonoApiHelper.ClassGetFieldFromName(kInvoker, '_handler');
-    let cur = MonoApiHelper.FieldGetValueObject(handler, self);
+	    let self = args[0];
+	    let handler = MonoApiHelper.ClassGetFieldFromName(kInvoker, '_handler');
+	    let cur = MonoApiHelper.FieldGetValueObject(handler, self);
 
-    if (cur.equals(pClientHandler)) return; // Already bypassed.
+	    if (cur.equals(pClientHandler)) return; // Already bypassed.
 
-    MonoApi.mono_field_set_value(self, handler, pClientHandler);
-    console.log(`[+]   Replaced with default handler @ ${pClientHandler}`);
-  }
-});
+	    MonoApi.mono_field_set_value(self, handler, pClientHandler);
+	    console.log(`[+]   Replaced with default handler @ ${pClientHandler}`);
+	}
+    });
+    console.log('[+] Hooked HttpMessageInvoker.SendAsync with DefaultHttpClientHandler technique');
+    hooked = true;
+} else {
+    console.log('[-] HttpClientHandler not found (Mono < 6.0?)');
+}
 
-console.log('[+] Done!\nMake sure you have a valid MITM CA installed on the device and have fun.');
+
+// Mono < 6.0: Hook the ServicePointManager. It should be harmless to do that in newer versions
+//             since the API is still there but unused.
+// [TODO] This is currently untested.
+let kServicePointManager = MonoApiHelper.ClassFromName(img, 'System.Net.ServicePointManager');
+var validationCallback = MonoApi.mono_class_get_property_from_name(c, Memory.allocUtf8String('ServerCertificateValidationCallback'))
+console.log(`[*] ServerCertificateValidationCallback @ ${validationCallback}`)
+let setter = MonoApi.mono_property_get_set_method(validationCallback)
+let getter = MonoApi.mono_property_get_set_method(validationcallback)
+
+if (setter && getter) {
+    MonoApiHelper.RuntimeInvoke(setter, NULL, NULL); // TODO: pArgs?
+    console.log('[+] Set ServerCertificateValidationCallback to NULL');
+    // TODO: Expose overload in frida-mono-api ?
+    pSet = MonoApi.mono_compile_method(setter)
+    pGet = MonoApi.mono_compile_method(getter)
+    // Hook get and set to always return / set NULL.
+    Interceptor.attach(pSet, {
+	onEnter: (args) => {
+	    args[1] = NULL;
+	}
+    });
+
+    Interceptor.attach(pGet, {
+	onLeave: (ret) => {
+	    ret = NULL; // TODO: 0?
+	}
+    });
+    console.log('[+] Hooked ServerCertificateValidationCallback with get/set technique')
+    hooked = true;
+} else {
+    console.log('[-] Getter/Setter not found for ServerCertificateValidationCallback')
+}
+
+if (hooked) console.log('[+] Done!\nMake sure you have a valid MITM CA installed on the device and have fun.');
+else console.log('[-] Failed to apply any bypass techniques... is this really Xamarin?')
